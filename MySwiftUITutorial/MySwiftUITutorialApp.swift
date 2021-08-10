3a5721f6f7460e71020512b75aed39f4fb544635
zoniha <awaawapurin@gmail.com>
2021-08-10 13:18:07 +0900

    Initial Commit

---
+++ MySwiftUITutorial/MySwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUITutorialApp.swift
+//  MySwiftUITutorial
+//
+//  Created by zoniha on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
